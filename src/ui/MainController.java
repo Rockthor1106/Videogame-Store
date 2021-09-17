74be21b188463c03fff00bc3c01d7f603c7d2f3e
jhan carlos carvajal bastidas <liceo.sur.andino2015@gmail.com>
2021-09-17 17:36:12 -0500

    Added a new class called
    "MainController" in package ui and I create a fxml document for the
    inputs screen

---
+++ src/ui/MainController.java
@@ -0,0 +1,32 @@
+package ui;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
+public class MainController {
+	    @FXML
+	    private TextField casesNumber;
+
+	    @FXML
+	    private BorderPane cashiersNumber;
+
+	    @FXML
+	    private TextField racksNumber;
+
+	    @FXML
+	    private TextArea racksDescription;
+
+	    @FXML
+	    private TextField clientsNumber;
+
+	    @FXML
+	    private TextArea gamesToBuy;
+
+	    @FXML
+	    void process(ActionEvent event) {
+
+	    }
+
+}
